604f371ef71d8ae70ee0d0ca84d4c2637b142ed3
meloalves <meloalvesjuliana@gmail.com>
2023-11-22 19:42:07 -0300

    final

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,6 +4,8 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
+import { ChatVideoButton } from "./chat-video-button";
+
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -27,6 +29,10 @@ export const ChatHeader = ({
         <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        {type === "conversation" && <ChatVideoButton />}
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
